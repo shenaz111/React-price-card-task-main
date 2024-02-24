02decf934a69a1d579c7e0dd8a23d73fa3714fe1
shenaz111 <shenazsheik777@gmail.com>
2024-02-24 21:38:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import PriceCard from "./PriceCard";
+function App() {
+  const plansArr = [
+    {
+      name: "FREE",
+      price: 0,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: false },
+        { name: "Dedicated Phone Support", enabled: false },
+        { name: "Free Subdomain", enabled: false },
+        { name: "Monthly Status Reports", enabled: false },
+      ],
+    },
+    {
+      name: "PLUS",
+      price: 9,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: true },
+        { name: "Dedicated Phone Support", enabled: true },
+        { name: "Free Subdomain", enabled: true },
+        { name: "Monthly Status Reports", enabled: false },
+      ],
+    },
+    {
+      name: "PRO",
+      price: 49,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: true },
+        { name: "Dedicated Phone Support", enabled: true },
+        { name: "Free Subdomain", enabled: true },
+        { name: "Monthly Status Reports", enabled: true },
+      ],
+    },
+  ];
+  return (
+    <section className="pricing py-5">
+    <div className="container">
+      <div className="row">
+      {plansArr.map((plan,index) => {
+        return (
+          <PriceCard key={index}
+            name={plan.name}
+            price={plan.price}
+            features={plan.features}
+          />
+        );
+      })}
+       </div>
+       </div>
+      </section>
+   
+  );
+}
+
+export default App;
